e1670b27098453097e78067764cf8bc6283b5128
mungera <aaron.munger@parivedasolutions.com>
2016-07-17 07:14:25 -0700

    started using jade

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
 var path = require ('path');
+var bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV || 'dev';
 
+app.set('views', path.join(__dirname, '/app/views'));
+app.set('view engine', 'jade');
+
 app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+app.get('*', function(req, res) {
+	res.render('index');
+});
 
 var port = 3000;
 app.listen(port, function() {
