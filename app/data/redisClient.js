82aa63da8b5dec6d322ed78de192a947356f0eec
mungera <aaron.munger@parivedasolutions.com>
2016-07-23 15:37:39 -0700

    updated redis connection

--- app/data/redisClient.js
+++ app/data/redisClient.js
@@ -1,5 +1,5 @@
-var redis = require("redis"),
-    client = redis.createClient();
+var redis = require("redis");
+var client = redis.createClient(process.env.REDIS_URL);
 
 client.on("error", function (err) {
     console.log("Error " + err);
