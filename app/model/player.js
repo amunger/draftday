5caa9a3be3db178b5c8aa1aa7c559871898ed4fa
mungera <aaron.munger@parivedasolutions.com>
2016-07-24 11:13:50 -0700

    rearrange players when selected

--- app/model/player.js
+++ app/model/player.js
@@ -8,6 +8,7 @@ var player = function(args){
   this.position = args.position && args.position.toUpperCase() || '';
   this.team = args.team && args.team.toUpperCase() || '';
   this.bye = args.bye || 0;
+  this.rank = args.rank * 100;
 }
 
 module.exports = player;
@@ -15,4 +16,9 @@ module.exports = player;
 var currentID = 1;
 var nextPlayerID = function () {
   return currentID++;
+}
+
+var getRank = function (rank){
+  var parsed = parseInt(rank * 100)
+  return parsed || 999999;
 }
--- public/js/core.js
+++ public/js/core.js
@@ -3,10 +3,12 @@ var draftDayApp = angular.module('draftDayApp', ['angularUtils.directives.dirPag
 function draftController($scope, $http) {
   $scope.selectedID = -1;
   $scope.leagueName = 'mockleague';
+  $scope.processing = false;
 
   $http.get('/api/players')
     .success(function(data) {
       $scope.players = data;
+      arrangePlayers();
     })
     .error(function(data) {
       console.log('Error: ' + data);
@@ -16,19 +18,24 @@ function draftController($scope, $http) {
     .success(function(data) {
         console.log(data);
         setTeams(data);
+        arrangePlayers();
       })
       .error(function(data) {
         console.log('Error: ' + data);
       });
 
   $scope.selectPlayer = function (playerID){
+    $scope.processing = true;
     $http.get('/api/selectPlayer/' + $scope.leagueName + '/' + playerID)
       .success(function(data) {
         console.log(data);
         setTeams(data);
+        $scope.processing = false;
+        arrangePlayers();
       })
       .error(function(data) {
         console.log('Error: ' + data);
+        $scope.processing = false;
       });
   }
 
@@ -37,6 +44,7 @@ function draftController($scope, $http) {
       .success(function(data) {
         console.log(data);
         setTeams(data);
+        arrangePlayers();
       })
       .error(function(data) {
         console.log('Error: ' + data);
@@ -81,5 +89,39 @@ function draftController($scope, $http) {
     $scope.currentPick = teams.shift();
     $scope.teams = teams;
   };
+
+  var arrangePlayers = function (){
+    var leagueTeams = $scope.teams || [];
+    if ($scope.players && $scope.currentPick){
+      $scope.players.map(function (player) {player.selected = null});
+      $scope.currentPick.players.forEach(function (player){
+        setPlayerAsSelected(player, $scope.currentPick.name);
+      });
+      leagueTeams.forEach(function (team){
+        team.players.forEach(function (player){
+          setPlayerAsSelected(player, team.name);
+        });
+      });
+    }
+  };
+
+  var setPlayerAsSelected = function (player, teamName){
+    var found = $scope.players.find(function(p) {return p.id === player.id});
+    var team = shortenName(teamName, 15);
+    if (found){
+      found.selected = team;
+    } else {
+      player.selected = team;
+      $players.push(player);
+    }
+  }
+
+  var shortenName = function (original, length){
+    if (original.length < 10){
+      return original;
+    } else {
+      return original.slice(0, length-1) + '...';
+    }
+  }
 
 }
