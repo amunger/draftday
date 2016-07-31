beea42f1ef718c70af32d8eb4ca78b1f311e2849
mungera <aaron.munger@parivedasolutions.com>
2016-07-31 11:51:02 -0700

    zig zag order

--- app/model/league.js
+++ app/model/league.js
@@ -6,7 +6,7 @@ var league = function (args){
   assert(args.name, 'A league needs a name');
   this.name = args.name
   this.teams = args.teams || [];
-  this.users = args.users || [];
+  this.currentPick = args.currentPick || 0;
 
   this.addTeam = function (team){
     this.teams.push(new Team(team));
@@ -15,18 +15,27 @@ var league = function (args){
   this.addPlayerToCurrentTeam = function(player){
     var teamList = this.teams;
     if(teamList.length > 0){
-      console.log('Adding ' + player.name + ' to ' + teamList[0].name);
-      teamList[0].players.push(player);
-      teamList.push(teamList.shift());
+      console.log('Adding ' + player.name + ' to ' + teamList[this.currentPick].name);
+      teamList[this.currentPick].players.push(player);
+      if (++this.currentPick > teamList.length-1){
+        this.teams.reverse();
+        this.currentPick = 0;
+      }
     }
   };
 
   this.undoLastPick = function(){
     var teamList = this.teams;
     if(teamList.length > 0){
-      teamList.unshift(teamList.pop());
-      if (teamList[0].players.length > 0){
-        var removed = teamList[0].players.pop();
+      if (this.currentPick == 0){
+        this.teams.reverse();
+        this.currentPick = teamList.length -1;
+      } else{
+        this.currentPick--;
+      }
+
+      if (teamList[this.currentPick].players.length > 0){
+        var removed = teamList[this.currentPick].players.pop();
         console.log('removing ' + removed.name + ' from ' + teamList[0].name);
       }
     }
