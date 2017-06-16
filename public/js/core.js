5b7d9180767bcf828306bba68cd924509633b9fb
mungera <aaron.munger@parivedasolutions.com>
2017-06-15 19:06:17 -0700

    set root scope vars on login

--- public/js/core.js
+++ public/js/core.js
@@ -16,12 +16,13 @@ draftDayApp.config(function($routeProvider) {
     })
 });
 
-draftDayApp.controller('loginController', ['$scope', '$cookies', 'socket', '$location', function($scope, $cookies, socket,  $location){
+draftDayApp.controller('loginController', ['$scope', '$cookies', 'socket', function($scope, $cookies, socket){
   $scope.joinLeague = function (){
     let leagueName = $scope.form.leagueName;
     let userName = $scope.form.userName;
     $cookies.put('leagueName', leagueName);
     $cookies.put('userName', userName);
+    setGlobals($scope.$parent, $cookies);
     socket.emit('joinRoom', {room: leagueName, userName: userName});
     $scope.form = {};
     location.href= '/#/draft';
@@ -29,9 +30,13 @@ draftDayApp.controller('loginController', ['$scope', '$cookies', 'socket', '$loc
 }]);
 
 draftDayApp.controller('mainCtrl', ['$scope', '$cookies', '$location', function($scope, $cookies, $location) {
-  $scope.leagueName = $cookies.get('leagueName');
-  $scope.userName = $cookies.get('userName');
+  setGlobals($scope, $cookies);
   $scope.isActive = function (viewLocation) {
         return viewLocation === $location.path();
   };
 }]);
+
+function setGlobals($scope, $cookies){
+  $scope.leagueName = $cookies.get('leagueName');
+  $scope.userName = $cookies.get('userName');
+}
