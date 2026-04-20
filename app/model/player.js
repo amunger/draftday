63e3d0a68a3c32f928d23efd260127d93f9d49a8
amunger <2019016+amunger@users.noreply.github.com>
2026-04-20 13:38:33 -0700

    change

--- app/model/player.js
+++ app/model/player.js
@@ -1,7 +1,7 @@
 'use strict'
 var assert = require('assert');
 
-var player = function(args){
+var player = function (args) {
   assert(args.name);
   this.id = args.id || nextPlayerID();
   this.name = args.name;
@@ -18,7 +18,7 @@ var nextPlayerID = function () {
   return currentID++;
 }
 
-var getRank = function (rank){
+var getRank = function (rank) {
   var parsed = parseInt(rank * 100)
-  return parsed || 999999;
+  return parsed || 9999999;
 }
